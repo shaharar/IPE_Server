a6353ee3d9f7c96beb32899e90177552d1fceeab
שחר ערד <shaharar@auth.ad.bgu.ac.il>
2019-05-22 00:15:59 +0300

    getPOIByName

--- Modules/POIs.js
+++ Modules/POIs.js
@@ -42,3 +42,58 @@ router.get('/getAllPOIs', function(req,res) {
             res.send(err)
         })
 })
+
+router.get('/getPOIByName/:name', function(req,res) {
+    var poiName = req.params.name;
+    var query1 = "SELECT * FROM POIs where Name = '" + poiName + "'";
+    DButilsAzure.execQuery(query1)
+        .then(function(result){
+            if(result.length != 0) {
+                var query2 = "update POIs set UsersWatching=" + (result[0].UsersWatching + 1) + " where Name='" + poiName + "'";
+                DButilsAzure.execQuery(query2)
+                    .then(function(result){
+                        var query3 = "SELECT * FROM POIs where Name = '" + poiName + "'";
+                        DButilsAzure.execQuery(query3)
+                            .then(function(result){
+                                res.status(200).send(result)
+                            })
+                    })
+            }
+            else {
+                res.status(400).send({success: false, message: "POI Name is invalid"})
+            }
+        })
+        .catch(function(err){
+            console.log(err)
+            res.send(err)
+        })
+})
+
+router.post('/addRank/', function (req, res) {
+    var poiID = req.body.ID;
+    var rank = req.body.Rank;
+    var review = req.body.Review;
+    var currDate = new Date().toISOString();
+    var totalRanks = 0;
+    DButilsAzure.execQuery("insert into POIsReviews (POI_ID,Username,Rank,Review,Date) values (" + poiID + "," + "'" + req.decoded.payload.username + ",'" + + rank + ",'" + review + "','" + currDate + "')")
+    .then(function (result) {
+        DButilsAzure.execQuery("select Rank from POIsReviews where POI_ID = " + poiID)
+        .then(function (result) {
+        for (let i = 0; i < result.length; i++) {
+            totalRanks += result[i].Rank;
+        }
+        avgRank = (totalRanks / (result.length * 5)) * 100;
+
+        DButilsAzure.execQuery("update POIs set Rank = " + avgRank + " where ID = " + poiID)
+        .then(function (result) {
+            res.status(200).send({success: true, message: "Review added successfuly"})
+            
+        })
+    }).catch(function (err) {
+        res.send(err);
+    })
+    }).catch(function(err)
+    {
+        res.status(400).send({success: false, message: "The same review from this user exists"})
+    })   
+})
